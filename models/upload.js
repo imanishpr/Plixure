3228829e9f22c3b6169c1d500dde362a94b85544
Manish <Manish>
2017-07-09 16:06:45 +0530

    test

--- models/upload.js
+++ models/upload.js
@@ -7,6 +7,25 @@ module.exports = {
 
     imageUpload: function (req, res, err) {
         if (req.header('X-FUTZ-SEC') == 'SorryForDelay-GetBackToYouSoon'){
+            console.log(req.body);
+            if((typeof albumId === 'undefined' || req.body.albumId === '')){
+                var query = "select pa_id from  ??  where pa_id = ? and status = ?";
+                var table = ["parcer_album" , userId , 18];
+                query = dbconfig.msql.format(query, table);
+                console.log(query);
+                dbconfig.connection.query(query, function (err, rows) {
+                    if (err) {
+                        res.json({"status": "failure", "data": err});
+                        return;
+                    } else {
+                        albumId = rows.pa_id;
+                    }
+                     
+                })
+            }
+
+
+            
             if(req.files.myImage.path && req.userId ){
                 cloudinary.uploader.upload(req.files.myImage.path, function(result) {
                     var userId  =   req.body.userId;
@@ -16,22 +35,6 @@ module.exports = {
                     var imgSUrl =   result.secure_url;
                     var imgPId  =   result.public_id;
                     var cDate   =   result.created_at;
-                    console.log(req.body);
-                    if((typeof albumId === 'undefined' || req.body.albumId === '')){
-                        var query = "select pa_id from  ??  where pa_id = ? and status = ?";
-                        var table = ["parcer_album" , userId , 18];
-                        query = dbconfig.msql.format(query, table);
-                        console.log(query);
-                        dbconfig.connection.query(query, function (err, rows) {
-                            if (err) {
-                                res.json({"status": "failure", "data": err});
-                                return;
-                            } else {
-                                albumId = rows.pa_id;
-                            }
-                             
-                        })
-                    }
                     var query = "insert into ??  values(?,?,?,?,?,?,?,?)";
                     var table = ["parcer_images" , "" , albumId, "", imgDesc ,cDate, 1, imgUrl, imgSUrl];
                     query = dbconfig.msql.format(query, table);
